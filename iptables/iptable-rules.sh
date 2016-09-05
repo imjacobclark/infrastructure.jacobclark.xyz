ee95eeec4d4695746621d65d90d4a1b37db42102
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-09-05 14:09:46 +0100

    Add iptables

---
+++ iptables/iptable-rules.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+/sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+/sbin/iptables -A INPUT -i lo -j ACCEPT
+# Switch on for debugging
+# /sbin/iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+/sbin/iptables -P INPUT -j DROP
+/sbin/iptables -P FORWARD -j REJECT
+/sbin/iptables -P OUTPUT -j ACCEPT
