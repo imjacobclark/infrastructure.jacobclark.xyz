49458f6fe95604cf5c38ea3842ced6f47cdc9c67
Jacob Clark <jacob.jh.clark@googlemail.com>
2017-02-20 11:31:14 +0000

    Update startup.sh

--- startup/startup.sh
+++ startup/startup.sh
@@ -2,4 +2,6 @@ curl http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address > /etc
 sed -i -e "s/{{PRIVATE_IP}}/$(cat /etc/privateip.txt)/g" /etc/prometheus.yml
 docker restart prometheus
 
+nohup jekyll serve --port 4001 > /dev/null &
+
 exit 0
