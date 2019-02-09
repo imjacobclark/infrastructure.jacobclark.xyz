eb3955a8803b0af8ebc127fe4599b9d33d17aa7c
Jacob Clark <jacob.clark@bbc.co.uk>
2019-02-09 14:49:37 +0000

    Resetting for v2

--- build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sed -e "s/{{API_KEY}}/$DIGITALOCEAN_API_TOKEN/g" packer/base.json | packer build -machine-readable -
-
--- startup/startup.sh
+++
@@ -1,5 +0,0 @@
-curl http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address > /etc/privateip.txt
-sed -i -e "s/{{PRIVATE_IP}}/$(cat /etc/privateip.txt)/g" /etc/prometheus.yml
-docker restart prometheus
-
-exit 0
